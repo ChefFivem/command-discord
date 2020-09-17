a36e4150965d704289236811f6efb9b4940f5865
Chef <67842030+ChefFivem@users.noreply.github.com>
2020-09-17 22:54:10 +0200

    Update client.lua

--- command_discord/client/client.lua
+++ command_discord/client/client.lua
@@ -1,5 +1,5 @@
 ESX = nil
 
 RegisterCommand("discord", function(source)
-    TriggerEvent("chatMessage", {255, 255, 255}, "Test discord")
+    TriggerEvent("chatMessage", {255, 255, 255}, "Join notre discord https://discord.gg/RNkZDwV")
 end)
